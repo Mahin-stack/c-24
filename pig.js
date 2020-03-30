b28c1c4487f741cbb4684d09fe44c3c63a59d4c9
Mahin-stack <62141543+Mahin-stack@users.noreply.github.com>
2020-03-30 15:57:27 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,25 @@
+class Pig {
+constructor(x,y){
+var options = {
+  restitution : 0.5,
+  friction :  1.2,
+  density : 1
+}
+this.body = Bodies.rectangle(x,y,45,45,options)
+this.width = 45
+this.height = 45 
+World.add(world,this.body)
+}
+display(){
+var pos = this.body.position
+var angle = this.body.angle
+push()
+translate(pos.x,pos.y)
+rotate(angle)
+ 
+rectMode(CENTER)
+fill("red")
+rect(0,0,this.width,this.height)
+pop()
+}
+}
